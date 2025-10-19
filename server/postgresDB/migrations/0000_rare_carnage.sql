16b15c1aa8b5616602cd398b2b06fa13dfb2d470
Ben Holland <benholland1024@gmail.com>
2025-10-19 12:47:33 +0200

    Hopefully fixing migration issue!

--- server/postgresDB/migrations/0000_rare_carnage.sql
+++ server/postgresDB/migrations/0000_rare_carnage.sql
@@ -1,3 +1,18 @@
+CREATE TABLE "password_reset_request_ips" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"ip_address" varchar(45) NOT NULL,
+	"created_at" timestamp DEFAULT now()
+);
+--> statement-breakpoint
+CREATE TABLE "password_reset_tokens" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"user_id" integer NOT NULL,
+	"token" varchar(255) NOT NULL,
+	"expires_at" timestamp NOT NULL,
+	"created_at" timestamp DEFAULT now(),
+	CONSTRAINT "password_reset_tokens_token_unique" UNIQUE("token")
+);
+--> statement-breakpoint
 CREATE TABLE "sessions" (
 	"id" varchar(255) PRIMARY KEY NOT NULL,
 	"user_id" integer NOT NULL,
@@ -36,6 +51,7 @@ CREATE TABLE "users" (
 	CONSTRAINT "users_email_unique" UNIQUE("email")
 );
 --> statement-breakpoint
+ALTER TABLE "password_reset_tokens" ADD CONSTRAINT "password_reset_tokens_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "sessions" ADD CONSTRAINT "sessions_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "databases" ADD CONSTRAINT "databases_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "user_table_positions" ADD CONSTRAINT "user_table_positions_database_id_databases_id_fk" FOREIGN KEY ("database_id") REFERENCES "public"."databases"("id") ON DELETE no action ON UPDATE no action;
